5922a6141d5a10f4896add66a967cc1f41789d79
jawon22 <137031459+jawon22@users.noreply.github.com>
2023-07-24 10:19:57 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle<=0 && angle >180) return 0;
+        
+        if(angle==180) return 4;
+        else if(angle ==90) return 2;
+        else if(angle >0 && angle<90) return 1;
+        else return 3;
+    }
+}
