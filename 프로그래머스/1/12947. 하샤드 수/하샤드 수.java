971831c5a4674939607e7a46bf653f5f748ba564
jawon22 <137031459+jawon22@users.noreply.github.com>
2024-07-22 22:39:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum=0;
+        
+        String str = Integer.toString(x);
+        
+        for(int i=0; i<str.length();i++){
+            sum += str.charAt(i)-'0';
+        }
+        
+        return x%sum == 0 ? true:false;
+    }
+}
